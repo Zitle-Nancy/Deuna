cdcf3a51b24dae5154943c384ae36b66b1629bc7
Zitle-Nancy <ancizj@hotmail.com>
2025-05-14 19:22:05 -0600

    [WIP]: add useStates for handle users data

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from "react";
 import { Avatar } from "../Avatar";
+import { UsersProps } from "./types";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const Card = () => {
+  const [users, setUsers] = useState<UsersProps | null>(null);
+  const [error, setError] = useState<string | null>(null);
   return (
     <li className="rounded-xl h-[300px] border-solid w-[500px]">
       <Avatar src={"https://randomuser.me/api/portraits/women/72.jpg"} />

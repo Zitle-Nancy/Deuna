d5d63a32696eff611c981b4d5568f82a79204fd0
Zitle-Nancy <ancizj@hotmail.com>
2025-07-09 13:27:02 -0600

    [feat]: WIP_fetching userInformation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import { Card } from "./components";
+import { useEffect, useState } from "react";
+import { Card, UserInformation } from "./components";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const dataUser = {
+  userName: "Nancy",
+  email: "test@gmail.com",
+  address: "debería ser opciona",
+  phoneNumber: "34563434",
+};
 
 const App = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [users, setUsers] = useState([]);
+
+  const fetchUserInformation = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        setError("Error in the API");
+      }
+      const results = await response.json();
+      setUsers(results);
+      setLoading(false);
+    } catch (error) {
+      setError(`Error: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInformation();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  console.log({ users });
   return (
     <ul className="h-[300px]">
       <Card />
+      <UserInformation dataUser={dataUser} />
     </ul>
   );
 };
